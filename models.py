ce7f08f0358a5ceab193c107a3dc18d104facf4a
Anthony0829cc <kung817@icloud.com>
2024-11-25 14:26:50 +0800

    changes

--- models.py
+++ models.py
@@ -3,9 +3,8 @@ from hashlib import sha256
 import flask_login
 
 class User(flask_login.UserMixin):
-    def __init__(self, username, email, password):
+    def __init__(self, username, password):
         # userid is the hashed username
         self.username = username
-        self.email = email
         self.password = password
         self.id = sha256(username.encode('utf-8')).hexdigest()
